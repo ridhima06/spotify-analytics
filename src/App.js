ec3efaad13e87d36fdf51bb17a1fc6154480026e
ridhima06 <ridhimaseth06@gmail.com>
2024-12-01 15:04:53 -0800

    Update test statement2

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Testing React
+          Testing React App stuff
         </a>
       </header>
     </div>
